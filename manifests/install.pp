076dfbccd2cc202832cde920bd167005dc29f38d
Aaron Hicks <hicksa@landcareresearch.co.nz>
2013-01-30 11:53:12 +1300

    Hmm, the puppet package should require the repository if it's used.

--- manifests/install.pp
+++ manifests/install.pp
@@ -12,21 +12,29 @@ class puppet::install(
 	$puppetlabs_repo,
 	$user_shell
 ) {
-	package{$puppet::params::puppet_package: ensure => installed}
-
-	user{'puppet':
-		ensure	=> present,
-		shell 	=> $user_shell,
-		require	=> Package[$puppet::params::puppet_package],
-	}
 
 	if $puppetlabs_repo == true {
+
 		apt::source { 'puppetlabs':
 		  location   => 'http://apt.puppetlabs.com',
 		  repos      => 'main',
 		  key        => '4BD6EC30',
 		  key_server => 'pgp.mit.edu',
 		}
+
+		package{$puppet::params::puppet_package:
+		 ensure => installed,
+		 require => Apt::Source['puppetlabs'],
+		}
+	} else {
+		package{$puppet::params::puppet_package: ensure => installed}
+	}
+	
+
+	user{'puppet':
+		ensure	=> present,
+		shell 	=> $user_shell,
+		require	=> Package[$puppet::params::puppet_package],
 	}
 
 	augeas{'puppet_main_config':
