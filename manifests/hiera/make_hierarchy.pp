0fffcf8766aeed929f557c19460af8dd3386d86c
Aaron Hicks <hicksa@landcareresearch.co.nz>
2013-02-11 15:32:09 +1300

    .json files can not be empty, must at least contain {}

--- manifests/hiera/make_hierarchy.pp
+++ manifests/hiera/make_hierarchy.pp
@@ -6,12 +6,15 @@ define puppet::hiera::make_hierarchy(
 	if $yaml {
 		file{"${puppet::install::hiera_datadir}/${name}.yaml":
 			ensure	=> file,
+			replace => false,
 		}
 	}
 
 	if $json {
 		file{"${puppet::install::hiera_datadir}/${name}.json":
 			ensure	=> file,
+			replace => false,
+			content => "{\n\n}\n",
 		}
 	}
 }
