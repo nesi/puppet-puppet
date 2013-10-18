123d601370fd922604d014b40cb8f5b676862330
Aaron Hicks <hicksa@landcareresearch.co.nz>
2013-10-18 14:09:49 +1300

    Test ensure parameter default and absent

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -6,7 +6,49 @@ describe 'puppet', :type => :class do
         :osfamily   => 'Debian',
       }
     end
-    it { should include_class('puppet::params') }
+    describe "with no parameters" do
+      it { should include_class('puppet::params') }
+      it { should contain_file('puppet_user_home').with(
+          'ensure'  => 'directory'
+        )
+      }
+      it { should contain_user('puppet_user').with(
+          'ensure'  => 'present'
+        )
+      }
+      it { should contain_file('puppet_conf_dir').with(
+          'ensure'  => 'directory'
+        )
+      }
+      it { should contain_file('puppet_conf').with(
+          'ensure'  => 'file'
+        )
+      }
+    end
+    describe "with ensure => absent" do
+      let :params do
+        {
+          :ensure => 'absent',
+        }
+      end
+      it { should include_class('puppet::params') }
+      it { should contain_file('puppet_user_home').with(
+          'ensure'  => 'absent'
+        )
+      }
+      it { should contain_user('puppet_user').with(
+          'ensure'  => 'absent'
+        )
+      }
+      it { should contain_file('puppet_conf_dir').with(
+          'ensure'  => 'absent'
+        )
+      }
+      it { should contain_file('puppet_conf').with(
+          'ensure'  => 'absent'
+        )
+      }
+    end
   end
 
   context "on a RedHat OS" do
