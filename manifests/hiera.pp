ccd1f1fc4452fdbfa68765664719a12089629ef7
Aaron Hicks <hicksa@landcareresearch.co.nz>
2013-10-25 12:22:55 +1300

    Refactor puppet::hiera for #2 removed puppet::hiera::install
    hiera doesn't need to handle environments, they should be described within the hiera hierarchy

--- manifests/hiera.pp
+++ manifests/hiera.pp
@@ -1,46 +1,95 @@
 # This manifest does the sanity checking in preparation of installing and
 # configuring hiera http://projects.puppetlabs.com/projects/hiera
 # Hiera is installed as part of the Puppet package, hence it is appropriate
-# Install and manage it as part of the Puppet installation int this Puppet
-# module
-#
-# This is _not_ required in Puppet 3.x!
 class puppet::hiera(
-  $ensure               = present,
-  $hiera_config_file    = false,
-  $hiera_config_source  = false,
-  $hiera_backend_yaml   = false,
-  $hiera_backend_json   = true,
-  $hiera_datadir        = false,
+  $ensure               = 'installed',
+  $hiera_config_file    = $puppet::params::hiera_config_file,
+  $hiera_datadir        = $puppet::params::hiera_datadir,
+  $hiera_config_source  = undef,
+  $hiera_backend        = 'yaml',
   $hiera_hierarchy      = ['commmon']
-  ) {
+  ) inherits puppet::params {
 
   require puppet
-  include puppet::params
 
-  if $hiera_config_file {
-    $config_file = $puppet::params::hiera_config_file
-  } else {
-    $config_file = $hiera_config_file
+  case $ensure {
+    /^installed$|^(\d+)?(\.(x|\*|\d+))?(\.(x|\*|\d+))?(|-(\S+))$/: {
+      $ensure_dir     = 'directory'
+      $ensure_file    = 'file'
+      $ensure_link    = 'link'
+      $ensure_present = 'present'
+    }
+    default: {
+      $ensure_dir     = 'absent'
+      $ensure_file    = 'absent'
+      $ensure_link    = 'absent'
+      $ensure_present = 'absent'
+    }
   }
 
-  if $hiera_datadir {
-    if $puppet::environments {
-      $datadir = $hiera_datadir
-    } else {
-      $datadir = $puppet::params::hiera_envs_datadir
+  # This package was most likely installed with Puppet, and this
+  # just provides a definition to work on. The require here just
+  # maintains relationships between puppet::hiera and puppet
+  package{'hiera':
+    ensure  => $ensure,
+    name    => $puppet::params::hiera_package,
+    require => Package['$puppet::params::puppet_package'],
+  }
+
+  if $ensure_present == 'present' {
+    $puppet_conf_hiera_change = "set master/hiera_config ${hiera_config_file}"
+  } else {
+    $puppet_conf_hiera_change = 'rm master/hiera_config'
+  }
+
+  # set the location of the hiera config file in the puppet config
+  augeas{'puppet_conf_hiera_config':
+    context => "/files$${puppet::puppet_conf_path}",
+    changes => [$puppet_conf_hiera_change],
+    require => File['puppet_conf','hiera_conf'],
+  }
+
+  augeas{'puppet_conf_heira_dedup':
+    context => "/files$${puppet::puppet_conf_path}",
+    changes => [
+      'rm main/hiera_config',
+      'rm agent/hiera_config',
+    ],
+    require => File['puppet_conf'],
+  }
+
+  if $hiera_config_source {
+    file{'hiera_conf':
+      ensure  => $ensure_file,
+      path    => $hiera_config_file,
+      source  => $hiera_config_source,
+      replace => false,
+      require => Package['hiera'],
     }
   } else {
-    $datadir = $puppet::params::hiera_datadir
+    file{'hiera_conf':
+      ensure  => $ensure_file,
+      path    => $hiera_config_file,
+      content => template('puppet/heira.yml.erb'),
+      replace => false,
+      require => Package['hiera'],
+    }
   }
 
-  class {'puppet::hiera::install':
-    ensure              => $ensure,
-    hiera_config_file   => $config_file,
-    hiera_config_source => $hiera_config_source,
-    hiera_backend_yaml  => $hiera_backend_yaml,
-    hiera_backend_json  => $hiera_backend_json,
-    hiera_datadir       => $datadir,
-    hiera_hierarchy     => $hiera_hierarchy,
+  file{'etc_hiera_conf':
+    ensure  => $ensure_link,
+    path    => '/etc/hiera,yaml',
+    target  => $hiera_config_file,
+    require => File['hiera_conf'],
   }
+
+  # Hiera does not require different data directories for different
+  # environments, environments should be handled within the hiera
+  # hierachy.
+  file{'hiera_datadir':
+    ensure  => $ensure_dir,
+    path    => $hiera_datadir,
+    require => Package['hiera'],
+  }
+
 }
--- manifests/params.pp
+++ manifests/params.pp
@@ -8,20 +8,23 @@ class puppet::params{
   $user                 = 'puppet'
   $gid                  = 'puppet'
   $user_home            = '/var/lib/puppet'
-  $var_dir               = '/var/lib/puppet'
-  $ssl_dir               = "${var_dir}/ssl"
-  $run_dir               = '/var/run/puppet'
-  $fact_path             = "${var_dir}/lib/facter"
-  $template_dir          = '$confdir/templates'
+  $var_dir              = '/var/lib/puppet'
+  $ssl_dir              = "${var_dir}/ssl"
+  $run_dir              = '/var/run/puppet'
+  $fact_path            = "${var_dir}/lib/facter"
+  $template_dir         = '$confdir/templates'
+  $hiera_config_file    = '$confdir/hiera.yaml'
+  $hiera_datadir        = "${conf_dir}/hieradata"
+  $hiera_package        = 'heira'
 
 
   case $::osfamily {
     Debian:{
       $app_dir              = '/usr/share/puppet'
-      $hiera_package        = 'heira-puppet'
-      $hiera_config_file    = "${conf_dir}/hiera.yaml"
+      
+      
       $hiera_config_content = "puppet${hiera_config_file}.erb"
-      $hiera_datadir        = "${conf_dir}/hieradata"
+      
       $hiera_envs_datadir   = "${conf_dir}/environments/${environment}/hieradata"
       $ruby_augeas_package  = 'libaugeas-ruby'
       $puppetmaster_package = 'puppetmaster-passenger'
