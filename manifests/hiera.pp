7bd0b328dad7369a1212663f942535e7f45b59dd
Aaron Hicks <hicksa@landcareresearch.co.nz>
2013-10-29 10:09:12 +1300

    puppet::hiera should reuse the puppet ensure value by default.

--- manifests/hiera.pp
+++ manifests/hiera.pp
@@ -2,7 +2,7 @@
 # configuring hiera http://projects.puppetlabs.com/projects/hiera
 # Hiera is installed as part of the Puppet package, hence it is appropriate
 class puppet::hiera(
-  $ensure               = 'installed',
+  $ensure               = $puppet::ensure,
   $hiera_config_file    = $puppet::params::hiera_config_file,
   $hiera_datadir        = $puppet::params::hiera_datadir,
   $hiera_config_source  = undef,
