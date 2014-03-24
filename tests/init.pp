f599449779e562edd1db72f648a9f3080dad189e
Aaron Hicks <hicksa@landcareresearch.co.nz>
2014-03-24 17:09:53 +1300

    Add conf and hiera classes

--- tests/init.pp
+++ tests/init.pp
@@ -13,3 +13,5 @@
 #     You should have received a copy of the GNU General Public License
 #     along with the puppet Puppet module.  If not, see <http://www.gnu.org/licenses/>.
 include puppet
+include puppet::conf
+include puppet::hiera
