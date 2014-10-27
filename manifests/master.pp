bb8507b9677351a7e314d32287781e48747d0a97
Tobias Brunner <tobias@tobru.ch>
2014-10-27 14:01:41 +0100

    add environmentpath and dns_alt_names parameters to master config

--- manifests/master.pp
+++ manifests/master.pp
@@ -10,7 +10,9 @@ class puppet::master (
   $reporturl            = undef,
   $storeconfigs         = undef,
   $storeconfigs_backend = undef,
-  $regenerate_certs     = true
+  $regenerate_certs     = true,
+  $environmentpath      = undef,
+  $dns_alt_names        = undef,
 ) inherits puppet::params {
 
   # Apache and Passenger need to be installed and set up beforehand
